af3ab9bcf0b5ad997caecdc57082a97dd1920418
Nikolay Olta <olservicemsc@mail.ru>
2024-01-08 00:18:15 +0600

    код не запускается и нет ошибок

--- olx_parser.py
+++ olx_parser.py
@@ -33,67 +33,67 @@ def translate_month_to_en(ru_month_name: str) -> str:
     return month_mapping[ru_month_name]
 
 
-'''Сложно воспринимать такую длинную функцию и с такой вложенностью циклов. Давай разобьём на несколько:
+def parser_page(url):
+    for page_num in range(1,26): 
+        category_page_html = request_html(url=f'{url}{page_num}') 
+        adt_urls = extract_adt_urls(category_page_html) 
+        for adt_url in adt_urls: 
+            yield parser_adt(adt_url)
 
-1. парсинг категории parser_category
-2. парсинг одной страницы в категории parser_page
-3. парсинг одного объявления на странице parser_adt'''
 
-def parser_page(url, count = 100):
-    count_error = 1
-    title_begin = None
-    title_str = None
-    for page in range(1,26):
-        title_begin = title_str
+def request_html(url):
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        data = soup.find_all('div', class_='css-1sw7q4x')
+        return data
+    except(requests.RequestException, ValueError, AttributeError):
+        print('error request_html')
+
+
+def extract_adt_urls(category_page_html):
+    adt_urls = []
+    for link in category_page_html:
         try:
-            response = requests.get(f'{url}{page}', timeout=5)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'lxml')
-            title_str = soup.find('title').text
-            if title_begin == title_str:
-                break  
-            data = soup.find_all('div', class_='css-1sw7q4x')
-            parser_adt(data, count_error)
-        except(requests.RequestException, ValueError, AttributeError):
-            print(f'___________________________________________________error page {count_error}___________________________________________________')
-            count_error += 1
-            continue
-
-
-
-def parser_adt(data, count_error):
-    try:    
-        for link in data:
             card_url = link.find('a').get('href')
             response_product = requests.get('https://www.olx.kz' + card_url, timeout=5)
-            response_product.raise_for_status()                
+            response_product.raise_for_status()
             soup_product = BeautifulSoup(response_product.text, 'lxml')
-            soup_product_number_id = soup_product.find('span', class_='css-12hdxwj er34gjf0').text                 
-            soup_product_name_salesman = soup_product.find('h4', class_='css-1lcz6o7 er34gjf0').text
-            soup_product_date_registered = soup_product.find('div', class_='css-16h6te1 er34gjf0').text
-            date_registered = parse_register_date(soup_product_date_registered)                
-            soup_product_date_of_last_visit = soup_product.find('span', class_='css-1t0qnkx').text
-            date_of_last_visit = parse_last_online_date(soup_product_date_of_last_visit)                
-            soup_product_date_posted = soup_product.find('span', class_='css-19yf5ek').text
-            # print(f'Проверка времени размещения: {soup_product_date_posted}')
-            date_posted = parse_published_date(soup_product_date_posted)                
-            soup_product_jpg = soup_product.find('img').get('src')                  
-            soup_product_title = soup_product.find('h4', class_='css-1juynto').text
-            soup_product_text = soup_product.find('div', class_='css-1t507yq er34gjf0').text
-            print(f'!!!!!!!!!!!!!!!!Объявление!!!!!!!!!!!!!!!!')
-            print(f'ID объявления: {soup_product_number_id}')
-            print(f'Ник продавца: {soup_product_name_salesman}')
-            print(f'Дата регистрации аккаунта: {date_registered}')
-            print(f'Время последнего посещения сайта: {date_of_last_visit}')
-            print(f'Дата размещения объявления: {date_posted}')
-            print(f'Ссылка на фотографию объявления: {soup_product_jpg}')
-            print(f'Название объявления: {soup_product_title}')
-            print(f'Описание объявления: {soup_product_text}')  
-            print('-----------------------')
+            adt_urls.append(soup_product)
+        except(requests.RequestException, ValueError, AttributeError):
+            print('error extract_adt_urls')
+            continue
+    print(adt_urls)
+
+def parser_adt(adt_url):
+    try:
+        soup_product_number_id = adt_url.find('span', class_='css-12hdxwj er34gjf0').text                 
+        soup_product_name_salesman = adt_url.find('h4', class_='css-1lcz6o7 er34gjf0').text
+        soup_product_date_registered = adt_url.find('div', class_='css-16h6te1 er34gjf0').text
+        date_registered = parse_register_date(soup_product_date_registered)                
+        soup_product_date_of_last_visit = adt_url.find('span', class_='css-1t0qnkx').text
+        date_of_last_visit = parse_last_online_date(soup_product_date_of_last_visit)                
+        soup_product_date_posted = adt_url.find('span', class_='css-19yf5ek').text
+        print(f'Проверка времени размещения: {soup_product_date_posted}')
+        date_posted = parse_published_date(soup_product_date_posted)                
+        soup_product_jpg = adt_url.find('img').get('src')                  
+        soup_product_title = adt_url.find('h4', class_='css-1juynto').text
+        soup_product_text = adt_url.find('div', class_='css-1t507yq er34gjf0').text
+        print(f'!!!!!!!!!!!!!!!!Объявление!!!!!!!!!!!!!!!!')
+        print(f'ID объявления: {soup_product_number_id}')
+        print(f'Ник продавца: {soup_product_name_salesman}')
+        print(f'Дата регистрации аккаунта: {date_registered}')
+        print(f'Время последнего посещения сайта: {date_of_last_visit}')
+        print(f'Дата размещения объявления: {date_posted}')
+        print(f'Ссылка на фотографию объявления: {soup_product_jpg}')
+        print(f'Название объявления: {soup_product_title}')
+        print(f'Описание объявления: {soup_product_text}')  
+        print('-----------------------')
   
     except(requests.RequestException, ValueError, AttributeError):
-        print(f'___________________________________________________error adt {count_error}___________________________________________________')
-        count_error += 1
+        print(f'___________________________________________________error adt___________________________________________________')
+        
         
         
 
